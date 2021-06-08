bb250f7b7130fc8fcd30609c008fe7373f9a7950
TheOverLordKotan <jdgalindo@itc.com.co>
2021-06-08 14:41:28 -0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/adarchitecture/main-itc-bvc
+
+go 1.16
+
+require github.com/stretchr/testify v1.7.0
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+"github.com/adarchitecture/bvc-kotan"
+)
+
+
+func main() {
+	fmt.Println("Hello World")
+	(bvc_kotan)
+}
